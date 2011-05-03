6adc4c148421e8b2f74c0fd5f0e4d40a965167e8
ashishcollabor <ashish.kumar@collabor.com>
2011-05-03 19:28:59 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diningin::Application.config.secret_token = '10a5c530864f964ab65896e44fbb1a3b73ce2e78da9aa5536235658af205890415fbbef33d19fb6dd735a9719781aca0e0d947ee08fe6c9b4ff848857085287e'
